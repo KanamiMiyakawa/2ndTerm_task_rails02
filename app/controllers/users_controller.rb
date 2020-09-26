c9f1f09ea3501c29ec7e3f58c049ab7b3c731c9c
kanami miyakawa <kedamap@yahoo.co.jp>
2020-09-26 10:03:22 +0000

    create user views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,4 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show]
-
-  def show
-  end
 
   def new
     @user = User.new
@@ -10,23 +6,18 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to user_path(user_params), notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.save
+      redirect_to user_path(@user.id), notice: 'User was successfully created.'
+    else
+      render :new
     end
   end
 
-  private
-
-  def set_user
+  def show
     @user = User.find(params[:id])
   end
+
+  private
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
