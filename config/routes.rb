8d113316b09db43bdb6c2e887da95a1f47266a40
kanami miyakawa <kedamap@yahoo.co.jp>
2020-09-28 01:22:04 +0000

    update sessions controller and routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
   resources :blogs
   resources :users, only: [:new, :create, :show]
+  resources :sessions, only: [:new, :create, :destroy]
 end
