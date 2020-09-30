8aaa1768ad35b1d60d7a44fa70f5e1d106db1c83
kanami miyakawa <kedamap@yahoo.co.jp>
2020-09-30 11:09:25 +0000

    update blog validation

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,11 @@
 class Blog < ApplicationRecord
   belongs_to :user
   mount_uploader :image, ImageUploader
+  validates :content_or_image, presence: true
+
+  private
+  
+  def content_or_image
+    content.presence or image.presence
+  end
 end
