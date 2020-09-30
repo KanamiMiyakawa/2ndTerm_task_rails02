e5044def74b753fa89616b2abc1e27a61c1947cf
kanami miyakawa <kedamap@yahoo.co.jp>
2020-09-30 10:15:04 +0000

    complete edit.html.erb

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -6,4 +6,12 @@ module BlogsHelper
       blog_path
     end
   end
+
+  def submit_button_value
+    if action_name == "index" || action_name == 'create' || action_name == 'confirm'
+      "確認画面へ"
+    elsif action_name == "edit"
+      "投稿する"
+    end
+  end
 end
